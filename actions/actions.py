287ec7d0cd98674613e5e88f0498c8e8e9ff1f16
Mohamed Mahir <mohamedmbh-wp23@student.tarc.edu.my>
2026-04-01 13:48:47 +0800

    almost fixed

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,40 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
+# Example dataset (replace with your JSONL later)
+COURSE_DB = {
+    "computer science": "Degree",
+    "information technology": "Diploma",
+    "business": "Degree"
+}
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionGetCourseInfo(Action):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self) -> Text:
+        return "action_get_course_info"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        course = tracker.get_slot("course")
+
+        if course:
+            course_lower = course.lower()
+
+            if course_lower in COURSE_DB:
+                programme = COURSE_DB[course_lower]
+                dispatcher.utter_message(
+                    text=f"{course.title()} is offered as a {programme} programme."
+                )
+            else:
+                dispatcher.utter_message(
+                    text="Sorry, I couldn't find that course."
+                )
+        else:
+            dispatcher.utter_message(
+                text="Please specify a course."
+            )
+
+        return []
